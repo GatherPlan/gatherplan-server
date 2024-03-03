5664db07ff5f12d62f511b15e8c9adff26ab3166
potential1205 <potential1205@naver.com>
2024-03-03 17:02:35 +0900

    2024-03-03

---
+++ src/main/java/com/example/GatherPlan/GatherPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.GatherPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatherPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GatherPlan/GatherPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GatherPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
