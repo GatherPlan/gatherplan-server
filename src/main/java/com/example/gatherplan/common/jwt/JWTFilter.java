ec718f5a299b236e3a68b0f0d8afead45dad9d41
potential1205 <potential1205@naver.com>
2024-03-14 20:54:05 +0900

    1. JWT 기능 구현: /api/v1/members/** 경로는 토큰 없이도 접근 가능하고 /api/v1/appointments/** 경로는 토큰이 필요한 접근이 가능하도록 구현, 2. 세션 로그인 방식 제거

--- src/main/java/com/example/gatherplan/common/jwt/JWTFilter.java
+++ src/main/java/com/example/gatherplan/common/jwt/JWTFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,7 +22,7 @@ public class JWTFilter extends OncePerRequestFilter {
     private final JWTUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         //request에서 Authorization 헤더를 찾음
         String authorization = request.getHeader("Authorization");
