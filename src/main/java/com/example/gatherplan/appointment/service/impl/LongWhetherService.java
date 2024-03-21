3d1bb43b42467cae5af9a93882a3768073c32ed2
potential1205 <dlwogns1205@naver.com>
2024-03-21 18:28:15 +0900

    외부 API 관련 의존관계/시큐리티 설정 추가, 컨트롤러 등록, 공공데이터 기상청 날씨 API 기능 작성 (추후 수정 필요)

---
+++ src/main/java/com/example/gatherplan/appointment/service/impl/LongWhetherService.java
@@ -0,0 +1,35 @@
+package com.example.gatherplan.appointment.service.impl;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class LongWhetherService {
+
+    private final WebClient webClient;
+
+    @Value("${external.api.longwhether.key}")
+    private String apiKey;
+
+    public LongWhetherService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://apis.data.go.kr/1360000/MidFcstInfoService")
+                .build();
+    }
+
+    public Mono<String> callExternalAPI() {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/getMidLandFcst")
+                        .queryParam("serviceKey", apiKey) // 인증키
+                        .queryParam("numOfRows", 10) // 페이지 당 결과 수
+                        .queryParam("pageNo", 1) // 페이지번호
+                        .queryParam("dataType", "JSON")
+                        .queryParam("regId", "11B00000") // 지역번호
+                        .queryParam("tmFc", "202403201800") // 발표시각
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+}
