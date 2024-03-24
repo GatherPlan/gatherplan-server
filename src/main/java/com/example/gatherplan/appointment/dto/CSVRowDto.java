d5cb17556dca91dcdf161d54101b45222bdcd1c2
potential1205 <dlwogns1205@naver.com>
2024-03-25 00:54:11 +0900

    Region 엔티티 관련(CSV Reader, Service, Dto) Java Doc 추가

--- src/main/java/com/example/gatherplan/appointment/dto/CSVRowDto.java
+++ src/main/java/com/example/gatherplan/appointment/dto/CSVRowDto.java
@@ -3,6 +3,10 @@ package com.example.gatherplan.appointment.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 로컬 DB에 법정동, 법정동 코드 관련 csv 파일을 불러오기 위한 임시 Dto입니다.
+ */
+
 @Getter
 @Setter
 public class CSVRowDto {
