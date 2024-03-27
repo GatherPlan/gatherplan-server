f2b86b5a78e3d55505391da9af16c6bbe84743da
potential1205 <dlwogns1205@naver.com>
2024-03-27 15:14:12 +0900

    응답 객체 어노테이션 정리 (@Geeter, @Builder 필수)

--- src/main/java/com/example/gatherplan/controller/vo/appointment/SearchDistrictResp.java
+++ src/main/java/com/example/gatherplan/controller/vo/appointment/SearchDistrictResp.java
@@ -1,12 +1,11 @@
 package com.example.gatherplan.controller.vo.appointment;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "약속 장소 행정구역 검색 응답 객체")
 public class SearchDistrictResp {
     @Schema(description = "행정구역 주소", example = "서울 성동구 용답동")
