47eb662716cd2506631185c8c269a56836651ae3
potential1205 <dlwogns1205@naver.com>
2024-04-05 02:45:58 +0900

    필드 하나인 dto 객체 제거 -> 필드 값 직접 넘기는 방법으로 변경, mapper 변경

--- src/main/java/com/example/gatherplan/appointment/service/AppointmentService.java
+++ src/main/java/com/example/gatherplan/appointment/service/AppointmentService.java
@@ -8,19 +8,17 @@ public interface AppointmentService {
 
     String registerAppointment(CreateAppointmentReqDto reqDto, String email);
 
-    boolean isUserParticipated(ParticipationStatusReqDto reqDto, String email);
+    boolean isUserParticipated(String appointmentCode, String email);
 
     List<AppointmentWithHostRespDto> retrieveAppointmentList(String email);
 
-    List<AppointmentWithHostRespDto> retrieveAppointmentSearchList(
-            AppointmentSearchReqDto reqDto, String email);
+    List<AppointmentWithHostRespDto> retrieveAppointmentSearchList(String keyword, String email);
 
-    AppointmentInfoRespDto retrieveAppointmentInfo(AppointmentInfoReqDto reqDto, String email);
+    AppointmentInfoRespDto retrieveAppointmentInfo(String appointmentCode, String email);
 
-    AppointmentParticipationInfoRespDto retrieveAppointmentParticipationInfo(
-            AppointmentParticipationInfoReqDto reqDto, String email);
+    AppointmentParticipationInfoRespDto retrieveAppointmentParticipationInfo(String appointmentCode, String email);
 
-    void deleteAppointment(DeleteAppointmentReqDto reqDto, String email);
+    void deleteAppointment(String appointmentCode, String email);
 
     void updateAppointment(UpdateAppointmentReqDto reqDto, String email);
 }
--- src/main/java/com/example/gatherplan/appointment/service/UserService.java
+++ src/main/java/com/example/gatherplan/appointment/service/UserService.java
@@ -1,10 +1,9 @@
 package com.example.gatherplan.appointment.service;
 
-import com.example.gatherplan.appointment.dto.AuthenticateEmailReqDto;
 import com.example.gatherplan.appointment.dto.CreateUserReqDto;
 
 public interface UserService {
-    void authenticateEmail(AuthenticateEmailReqDto authenticateEmailReqDto);
+    void authenticateEmail(String email);
 
     void joinUser(CreateUserReqDto createUserReqDto);
 
