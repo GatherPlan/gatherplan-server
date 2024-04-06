d2ef0f1c8590ff340cd12905050a14027f9c71cb
potential1205 <dlwogns1205@naver.com>
2024-04-07 05:19:12 +0900

    응답 데이터 필드 순서 조정

--- src/main/java/com/example/gatherplan/appointment/dto/AppointmentWithHostRespDto.java
+++ src/main/java/com/example/gatherplan/appointment/dto/AppointmentWithHostRespDto.java
@@ -1,17 +1,20 @@
 package com.example.gatherplan.appointment.dto;
 
 import com.example.gatherplan.appointment.enums.AppointmentState;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentWithHostRespDto {
-
-    private String appointmentName;
     private String hostName;
-    private AppointmentState appointmentState;
     private String appointmentCode;
+    private String appointmentName;
+    private AppointmentState appointmentState;
+
 
 }
