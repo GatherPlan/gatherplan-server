f320b818c13967dda1532cfec81b085bcbed7bc8
박승일 <bob8dod@gmail.com>
2024-04-13 03:21:16 +0900

    코드 리뷰 반영 및 충돌 해결

--- src/main/java/com/example/gatherplan/appointment/dto/AppointmentRespDto.java
+++ src/main/java/com/example/gatherplan/appointment/dto/AppointmentRespDto.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class AppointmentRespDto {
     private String appointmentName;
     private Address address;
