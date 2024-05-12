e7e718030ce04e9a2d447e611c8c598161285861
이재훈 <dlwogns1205@naver.com>
2024-05-13 00:42:40 +0900

    비회원의 약속 참여 취소하기 기능 추가, 약속 정보 조회하기 약속 후보 날짜 필드 추가

--- src/main/java/com/example/gatherplan/appointment/dto/TempAppointmentInfoRespDto.java
+++ src/main/java/com/example/gatherplan/appointment/dto/TempAppointmentInfoRespDto.java
@@ -3,6 +3,9 @@ package com.example.gatherplan.appointment.dto;
 import com.example.gatherplan.common.unit.Address;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,4 +21,6 @@ public class TempAppointmentInfoRespDto {
     private Address address;
 
     private String notice;
+
+    private List<LocalDate> candidateDateList;
 }
