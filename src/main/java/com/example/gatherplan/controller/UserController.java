e1208e9334e7dafe93da2c83cf494f62a5fafd97
박승일 <bob8dod@gmail.com>
2024-05-28 21:42:11 +0900

    #38 이슈 확인 테스트용 코드 생성

--- src/main/java/com/example/gatherplan/controller/UserController.java
+++ src/main/java/com/example/gatherplan/controller/UserController.java
@@ -13,10 +13,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -52,6 +49,11 @@ public class UserController {
         return ResponseEntity.ok(
                 BooleanResp.success()
         );
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<BooleanResp> test() {
+        return ResponseEntity.ok(BooleanResp.success());
     }
 }
 
