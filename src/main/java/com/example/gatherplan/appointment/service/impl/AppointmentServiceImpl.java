b7067d82983514322cdd65033c65c18de6a5734a
이재훈 <potential1205@naver.com>
2024-06-03 21:15:25 +0900

    #48 AppointmentServiceImpl -> CustomUserAppointmentMappingRepository 미사용으로 인한 의존 관계 삭제

--- src/main/java/com/example/gatherplan/appointment/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/example/gatherplan/appointment/service/impl/AppointmentServiceImpl.java
@@ -41,7 +41,6 @@ public class AppointmentServiceImpl implements AppointmentService {
 
     private final CustomUserRepository customUserRepository;
     private final CustomAppointmentRepository customAppointmentRepository;
-    private final CustomUserAppointmentMappingRepository customUserAppointmentMappingRepository;
 
     @Override
     @Transactional
