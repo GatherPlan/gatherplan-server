565c9176cba49f20962cfc71415305ea6959b6b8
이재훈 <potential1205@naver.com>
2024-06-10 15:54:26 +0900

    #59 약속 참여 변경하기 : 참여 변경시 해당 약속이 미확정 상태일 경우에만 선택되도록 수정

--- src/main/java/com/example/gatherplan/appointment/repository/AppointmentRepository.java
+++ src/main/java/com/example/gatherplan/appointment/repository/AppointmentRepository.java
@@ -1,5 +1,6 @@
 package com.example.gatherplan.appointment.repository;
 
+import com.example.gatherplan.appointment.enums.AppointmentState;
 import com.example.gatherplan.appointment.repository.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
     Optional<Appointment> findByAppointmentCode(String appointmentCode);
+
+    Optional<Appointment> findByAppointmentCodeAndAppointmentState(String appointmentCode, AppointmentState appointmentState);
 }
