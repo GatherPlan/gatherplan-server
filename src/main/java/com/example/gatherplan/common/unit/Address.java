c26e23b646b374bdd99182db53fcb7fd8e138e40
이재훈 <potential1205@naver.com>
2024-06-10 16:40:41 +0900

    #61 약속 만들기 : Address 객체 각 필드에 @Nullable, @NotNull 적용

--- src/main/java/com/example/gatherplan/common/unit/Address.java
+++ src/main/java/com/example/gatherplan/common/unit/Address.java
@@ -1,9 +1,11 @@
 package com.example.gatherplan.common.unit;
 
 import com.example.gatherplan.common.enums.LocationType;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.Comment;
 
@@ -16,15 +18,19 @@ public class Address {
 
     @Enumerated(EnumType.STRING)
     @Comment("형식 구분 값 (상세주소/행정구역)")
+    @NotNull
     private LocationType locationType;
 
     @Comment("상세주소/행정구역 명")
+    @NotNull
     private String fullAddress;
 
     @Comment("장소 이름")
+    @Nullable
     private String placeName;
 
     @Comment("장소 상세 정보 링크)")
+    @Nullable
     private String placeUrl;
 
 }
