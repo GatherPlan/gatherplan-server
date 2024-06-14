ce53f08c55ef10d7737778c9058a416da97978d4
이재훈 <potential1205@naver.com>
2024-06-14 11:58:43 +0900

    #80 사용하지 않는 레포지토리 클래스, 메서드 제거 :CustomUserAppointmentMappingRepository, CustomUserAppointmentMappingRepositoryImpl, findAllByUserSeq()

--- src/main/java/com/example/gatherplan/appointment/repository/CustomUserAppointmentMappingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gatherplan.appointment.repository;
-
-import java.util.List;
-
-
-public interface CustomUserAppointmentMappingRepository {
-
-    List<String> findAllByUserSeq(Long userId);
-}
