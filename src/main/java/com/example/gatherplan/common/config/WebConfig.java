9d9150a56f56e33b5b0e80a9194885638e59d220
이재훈 <potential1205@naver.com>
2024-06-23 22:59:25 +0900

    test1

---
+++ src/main/java/com/example/gatherplan/common/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.gatherplan.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
