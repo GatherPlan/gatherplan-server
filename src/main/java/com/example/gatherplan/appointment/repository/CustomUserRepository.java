0b602b9e6d2185f1bc27b64f34c53596b414e9be
이재훈 <potential1205@naver.com>
2024-07-01 19:16:37 +0900

    #107 미사용 메서드 제거

--- src/main/java/com/example/gatherplan/appointment/repository/CustomUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gatherplan.appointment.repository;
-
-import java.util.List;
-
-public interface CustomUserRepository {
-
-    List<String> findAllUserNameByAppointmentCode(String appointmentCode);
-}
