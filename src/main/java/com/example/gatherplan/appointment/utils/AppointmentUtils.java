315ca344ea32fb10151a37b4a60a9d308f5a0537
박승일 <bob8dod@gmail.com>
2024-07-10 21:32:05 +0900

    #111 종료시간 계산 (23:59 <-> 24:00) 이 모두 AppointmentUtils 에서 이루어질 수 있도록 수정 [리팩토링]

--- src/main/java/com/example/gatherplan/appointment/utils/AppointmentUtils.java
+++ src/main/java/com/example/gatherplan/appointment/utils/AppointmentUtils.java
@@ -93,7 +93,7 @@ public class AppointmentUtils {
     }
 
     // 24시간 동안 1시간 단위로 해당 조합의 멤버 모두 참여 가능한 시간 리스트 구하기 ex) [1,2,3,5,6,10, ...]
-    private static List<Integer> getTimeListWhenAllAvailable(
+    private List<Integer> getTimeListWhenAllAvailable(
             LocalDate candidateDate, List<List<SelectedDateTime>> filteredSelectedDatesList,
             List<UserAppointmentMapping> filteredParticipationInfoList
     ) {
@@ -105,7 +105,7 @@ public class AppointmentUtils {
                 for (SelectedDateTime selectedDateTime : selectedDateTimes) {
                     LocalDate date = selectedDateTime.getSelectedDate();
                     int startHour = selectedDateTime.getSelectedStartTime().getHour();
-                    int endHour = getEndTimeHour(selectedDateTime.getSelectedEndTime());
+                    int endHour = localEndTimeToHour(selectedDateTime.getSelectedEndTime());
 
                     if (candidateDate.equals(date) && (startHour <= nowHour && nowHour <= endHour)) {
                         includedCount++;
@@ -121,7 +121,7 @@ public class AppointmentUtils {
     }
 
     // 시간 리스트에서 연속적인 시간 그룹 찾기 ex) [1,2,3,4,10,11,18] -> [[1,2,3,4],[10,11]] // 18은 들어가면 안됨 -> start != end
-    private static void findContinuousTimeAndAddResult(
+    private void findContinuousTimeAndAddResult(
             Set<String> combination, LocalDate candidateDate, List<Integer> timeList,
             List<AppointmentCandidateInfo> candidateInfoList, List<UserParticipationInfo> userParticipationInfoList) {
         int start = timeList.get(0);
@@ -152,8 +152,8 @@ public class AppointmentUtils {
     }
 
     // 현재 나온 결과가 이미 반영된 결과인지 확인
-    private static boolean isDuplicated(Set<String> combination, LocalDate candidateDate,
-                                        List<AppointmentCandidateInfo> candidateInfoList, int start, int end) {
+    private boolean isDuplicated(Set<String> combination, LocalDate candidateDate,
+                                 List<AppointmentCandidateInfo> candidateInfoList, int start, int end) {
         return candidateInfoList.stream()
                 .anyMatch(
                         candidateDateInfo -> {
@@ -176,13 +176,7 @@ public class AppointmentUtils {
                 );
     }
 
-    private int getEndTimeHour(LocalTime endTime) {
-        int hour = endTime.getHour();
-        int minute = endTime.getMinute();
-        return (hour == 23 && minute == 59) ? 24 : hour;
-    }
-
-    public List<UserParticipationInfo> retrieveUserParticipationInfoList(List<UserAppointmentMapping> userAppointmentMappingList, String hostName) {
+    public List<UserParticipationInfo> retrieveuserParticipationInfoList(List<UserAppointmentMapping> userAppointmentMappingList, String hostName) {
         return userAppointmentMappingList.stream()
                 .filter(mapping -> UserRole.GUEST.equals(mapping.getUserRole()))
                 .map(mapping -> UserParticipationInfo.builder()
@@ -194,5 +188,28 @@ public class AppointmentUtils {
                 .toList();
     }
 
+    /**
+     * 종료 LocalTime 시간 변환
+     * LocalTime 에 24시가 없으므로 종료 시간 중 하나인 23시 59분의 시간반환을 24로 반환해 줌
+     *
+     * @param endTime 약속종료시간
+     * @return 시간(HH)
+     */
+    public int localEndTimeToHour(LocalTime endTime) {
+        int endHour = endTime.getHour();
+        int endMinute = endTime.getMinute();
+        return (endHour == 23 && endMinute == 59) ? 24 : endHour;
+    }
+
+    /**
+     * 종료 시간(HH) LocalTIme 변환
+     * LocalTime 에 24시가 없으므로 시간이 24시 일 경우 23시 59분의 LocalTime 으로 반환해 줌
+     *
+     * @param endHour 시간
+     * @return 종료시각
+     */
+    public LocalTime hourToLocalEndTime(int endHour) {
+        return endHour == 24 ? LocalTime.of(23, 59) : LocalTime.of(endHour, 0);
+    }
 
 }
