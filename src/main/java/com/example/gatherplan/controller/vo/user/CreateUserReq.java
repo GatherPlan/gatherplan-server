9c31145255902fa96eca060d88399a930b215f1c
이재훈 <potential1205@naver.com>
2024-08-11 18:40:23 +0900

    #117 vo/user,tempuser /req,resp 객체 어노테이션 점검 및 수정 : @NotEmpty, @NotNull 적용 / @Schema 위치 변경

--- src/main/java/com/example/gatherplan/controller/vo/user/CreateUserReq.java
+++ src/main/java/com/example/gatherplan/controller/vo/user/CreateUserReq.java
@@ -11,27 +11,27 @@ import lombok.*;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-@Schema(description = "회원가입 요청 객체")
+@Schema(description = "회원가입 요청")
 public class CreateUserReq {
 
-    @Schema(description = "이메일", example = "email@example.com")
     @NotBlank(message = "이메일은 공백이 될 수 없습니다.")
     @Email(message = "이메일 형식이 맞지 않습니다.")
+    @Schema(description = "이메일", example = "email@example.com")
     private String email;
 
-    @Schema(description = "인증코드(6자리)", example = "123456")
     @NotBlank(message = "인증번호는 공백이 될 수 없습니다.")
     @Size(min = 6, max = 6, message = "인증번호는 6자입니다.")
+    @Schema(description = "인증코드(6자리)", example = "123456")
     private String authCode;
 
-    @Schema(description = "회원 닉네임", example = "홍길동")
     @NotBlank(message = "이름은 공백이 될 수 없습니다.")
     @Size(min = 2, max = 6, message = "이름은 2자 이상 6자 이하여야 합니다.")
+    @Schema(description = "회원 닉네임", example = "홍길동")
     private String name;
 
-    @Schema(description = "회원 비밀번호", example = "abcd1234")
     @NotBlank(message = "비밀번호는 공백이 될 수 없습니다.")
     @Size(min = 4, max = 12, message = "비밀번호는 4자 이상 12자 이하여야 합니다.")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).+$", message = "비밀번호는 영문자, 숫자를 적어도 하나씩 포함해야 합니다.")
+    @Schema(description = "회원 비밀번호", example = "abcd1234")
     private String password;
 }
