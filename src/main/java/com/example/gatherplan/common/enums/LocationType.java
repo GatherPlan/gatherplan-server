bcd3201890c2d6e168b5569d20c8c3caff894317
박승일/디지털채널서비스팀/HAE <bob8dod@hyundai-autoever.com>
2024-10-25 22:17:45 +0900

    #136 약속 장소 타입 추가 (직접입력 - CUSTOM_ADDRESS)

--- src/main/java/com/example/gatherplan/common/enums/LocationType.java
+++ src/main/java/com/example/gatherplan/common/enums/LocationType.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum LocationType {
     DETAIL_ADDRESS("상세 주소"),
-    DISTRICT("행정 구역");
+    DISTRICT("행정 구역"),
+    CUSTOM_ADDRESS("직접 입력 주소");
 
     private final String description;
 }
