28817203e3064356aa201f4c21d77c43513569f0
박승일/디지털채널서비스팀/HAE <bob8dod@hyundai-autoever.com>
2024-10-30 21:33:59 +0900

    #137 Exception 발생 시 Request Body 로그 남기도록 Filter 설정 추가

---
+++ src/main/java/com/example/gatherplan/common/filter/RequestWrapper.java
@@ -0,0 +1,71 @@
+package com.example.gatherplan.common.filter;
+
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import lombok.Getter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Slf4j
+@Getter
+public class RequestWrapper extends HttpServletRequestWrapper {
+    //Use this method to read the request body N times
+    private final String body;
+
+    @SneakyThrows
+    public RequestWrapper(HttpServletRequest request) {
+        super(request);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = request.getReader()) {
+            char[] charBuffer = new char[128];
+            int bytesRead;
+            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                stringBuilder.append(charBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            log.error("RequestWrapper occurred exception", e);
+            throw e;
+        }
+
+        body = stringBuilder.toString();
+    }
+
+    @Override
+    public ServletInputStream getInputStream() {
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
+        return new ServletInputStream() {
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+
+            @Override
+            public boolean isReady() {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener) {
+                throw new UnsupportedOperationException();
+            }
+
+            public int read() {
+                return byteArrayInputStream.read();
+            }
+        };
+    }
+
+    @Override
+    public BufferedReader getReader() {
+        return new BufferedReader(new InputStreamReader(this.getInputStream()));
+    }
+
+}
