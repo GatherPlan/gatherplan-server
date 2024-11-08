d1a5b871cbed82edbbe7ac9cd59df20c3a588b50
이재훈 <potential1205@naver.com>
2024-11-08 14:59:55 +0900

    #149 불필요한 import 제거

--- src/main/java/com/example/gatherplan/appointment/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/gatherplan/appointment/service/impl/UserServiceImpl.java
@@ -33,7 +33,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Random;
