21faa3640638c04fbdcb834b678966c7fa7c2b05
박승일 <bob8dod@gmail.com>
2024-11-09 21:24:46 +0900

    #156 요청 필드 locationType 제거 및 주소 가공 로직 추가 적

--- src/main/java/com/example/gatherplan/region/repository/RegionRepository.java
+++ src/main/java/com/example/gatherplan/region/repository/RegionRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RegionRepository extends JpaRepository<Region, Long> {
 
     Page<Region> findByAddressContaining(String keyword, Pageable pageable);
 
+    Optional<Region> findByAddress(String keyword);
 }
