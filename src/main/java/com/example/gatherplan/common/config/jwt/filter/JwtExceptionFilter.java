f91b4ae5829f9d6a22955a3685f98e92269fb04b
이재훈 <potential1205@naver.com>
2024-11-10 21:14:48 +0900

    #159 JwtExceptionFilter 예외 처리 응답에 Object Mapper 적용

--- src/main/java/com/example/gatherplan/common/config/jwt/filter/JwtExceptionFilter.java
+++ src/main/java/com/example/gatherplan/common/config/jwt/filter/JwtExceptionFilter.java
@@ -2,19 +2,25 @@ package com.example.gatherplan.common.config.jwt.filter;
 
 import com.example.gatherplan.common.config.jwt.exception.JwtTokenException;
 import com.example.gatherplan.common.exception.ErrorCode;
+import com.example.gatherplan.controller.exception.ErrorResp;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @Slf4j
 public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
@@ -33,12 +39,7 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
-        String jsonResponse = String.format("""
-            {
-                "code": "%s",
-                "message": "%s"
-            }
-            """, errorCode.getCode(), errorCode.getMessage());
+        String jsonResponse = objectMapper.writeValueAsString(ErrorResp.of(errorCode));
 
         response.getWriter().write(jsonResponse);
     }
